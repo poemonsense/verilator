15c799add16566d2958cd6aa77e6aab3e730ef9e
Yinan Xu <xuyinan@ict.ac.cn>
2023-06-13 14:19:27 +0800

    Allow the user to explicitly reset the thread pool
    It's difficult to use multi-threading with fork(). The Verilator
    thread pool belongs to the parent process only after fork() is called.
    This commit adds a member function to allow the user explicitly
    resetting the thread pool from the child process after fork().
    Only after the thread pool is created, the simulation could continue.
    
    This is dirty but I don't know other ways to do this.
    
    One clean way is to use pthread_atfork. However, it does not accept
    C++ lambda functions. We need lambdas instead of function pointers
    to catch the `this` context. Therefore, this is not possible.

--- include/verilated.h
+++ include/verilated.h
@@ -580,6 +580,7 @@ public:
     void addModel(VerilatedModel*);
 
     VerilatedVirtualBase* threadPoolp();
+    VerilatedVirtualBase* resetThreadPoolp();
     VerilatedVirtualBase*
     enableExecutionProfiler(VerilatedVirtualBase* (*construct)(VerilatedContext&));
 
